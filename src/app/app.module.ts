99acc2e836fa604a7bc42555e4f8fe7c5c931080
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2024-05-29 06:01:26 +0200

    navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToDoComponent } from './to-do/to-do.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
-import { HederComponent } from './heder/heder.component';
 import { HeaderComponent } from './header/header.component';
 import { CompletedToDoComponent } from './completed-to-do/completed-to-do.component';
 import { ActiveToDoComponent } from './active-to-do/active-to-do.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToDoComponent,
     ToDoListComponent,
-    HederComponent,
     HeaderComponent,
     CompletedToDoComponent,
     ActiveToDoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: "active-to-do", component:ActiveToDoComponent},
+      {path: "", component:ToDoListComponent},
+      {path: "to-do-list", component: ToDoListComponent},
+      {path: "completed-to-do", component: CompletedToDoComponent}
+    ])
   ],
   providers: [
     provideClientHydration()
